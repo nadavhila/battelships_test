9b0a80fbd01fb40a91e757a07f2a255352648e2a
eladna <nelad@campus.technion.ac.il>
2023-04-13 17:36:00 +0300

    array targil

---
+++ test1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class test1 {
+    Scanner scanner = new Scanner(system.in);
+    system.out.println("please enter your name");
+    string name=scanner.nextLine();
+    system.out.println("please enter your favourite food");
+    string food=scanner.nextLine();
+    system.out.println("please enter your age");
+    string age=scanner.nextLine();
+    system.out.println("hello"+name!);
+    system.out.println("you love to eat"+food+"and you are"+age+"year old");
+
+
+}
